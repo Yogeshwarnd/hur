5b3135cc0830b88e36b8199fc9cc6bc10bac5315
N D Yogeshwar <192465067.simats@saveetha.com>
2025-12-31 09:25:23 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,14 @@
+const stone = document.getElementById("stone");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const scoreofYou = document.getElementById("scoreofyou");
+const scoreofBot = document.getElementById("scoreofbot");
+
+let scoreYou = 0;
+let scoreBot = 0;
+
+let bot=Math.floor(Math.random()*3)
+console.log(bot)
+
+
+
